387d7236e53af9910cfb648cf6bfb067fce3ce20
IHAVENOARMS <45311996+IHAVENOARMS@users.noreply.github.com>
2022-10-09 14:29:29 +0200

    added new models

---
+++ models/batch.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const Batch = mongoose.model(
+  'batch',
+  mongoose.Schema({
+    number: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 3000,
+      unique: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 3000,
+      unique: true,
+    },
+    name: { type: String, minLength: 1, maxLength: 100 },
+    channelId: { type: String, min: 5, max: 20, unique: true },
+    groupChatId: { type: String, min: 5, max: 20, unique: true },
+  })
+);
+
+const validate = (batch) => {
+  const joiSchema = Joi.object({
+    number: Joi.number().min(1).max(3000).required(),
+    year: Joi.number().min(1).max(3000).required(),
+    name: Joi.string().min(1).max(100),
+    channelId: Joi.string().min(5).max(20),
+    groupChatId: Joi.string().min(5).max(20),
+  });
+  return joiSchema.validate(batch);
+};
+
+module.exports = { Batch, validate };
--- models/user.js
+++ models/user.js
@@ -1,2 +1,53 @@
-const mongoose = require("mongoose");
-const Joi = require("joi");
+const mongoose = require('mongoose');
+const { countries } = require('countries-list');
+const Joi = require('joi');
+const objectId = require('joi-objectid')(Joi);
+const {
+  arabicName,
+  englishName,
+  countryCode,
+  gender,
+} = require('../utils/joiValidators');
+
+const countryCodeList = Object.keys(countries).map((c) =>
+  c.toLocaleLowerCase()
+);
+
+const batchSchema = mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Batch',
+    required: true,
+  },
+  number: { type: Number, min: 1, max: 3000, required: true },
+});
+
+const roleSchema = mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, ref: 'Role', required: true },
+  name: { type: String, required: true },
+});
+
+const User = mongoose.model(
+  'user',
+  mongoose.Schema({
+    arabicName: { type: String, required: true, minLength: 8, maxLength: 40 },
+    englishName: { type: String, required: true, minLength: 8, maxLength: 40 },
+    gender: { type: String, required: true, enum: ['m', 'f'] },
+    country: { type: String, required: true, enum: countryCodeList },
+    role: { type: roleSchema, required: true },
+    batch: {
+      type: batchSchema,
+      required: true,
+    },
+  })
+);
+
+const validate = () => {
+  const joiSchema = Joi.object({
+    arabicName: arabicName().min(8).max(40).required(),
+    englishName: englishName().min(8).max(40).required(),
+    gender: gender().required(),
+    country: countryCode().required(),
+    batchId: objectId(),
+  });
+};
