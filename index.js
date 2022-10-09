6260d2fac68c4304f976e194b70042db36161002
IHAVENOARMS <45311996+IHAVENOARMS@users.noreply.github.com>
2022-10-09 14:29:52 +0200

    changed test code

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-require("dotenv").config();
-const config = require("config");
-const { json } = require("express");
-const express = require("express");
+require('dotenv').config();
+const config = require('config');
+const mongoose = require('mongoose');
+const express = require('express');
 const app = express();
+const helmet = require('helmet');
+const cors = require('cors');
 
-app.use(json);
-app;
+const apiEndPoint = config.get('apiEndPoint');
+const batchRouter = require('./routes/batches');
 
-app.listen(config.get("testPort"), () => {
-  console.log(`Started listening at port ${config.get("testPort")}...`);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(apiEndPoint + 'batches', batchRouter);
+
+mongoose.connect('mongodb://localhost/aun-manager', () => {
+  console.log('Successfully connected to database');
+});
+
+app.listen(config.get('testPort'), () => {
+  console.log(`Started listening at port ${config.get('testPort')}...`);
 });
